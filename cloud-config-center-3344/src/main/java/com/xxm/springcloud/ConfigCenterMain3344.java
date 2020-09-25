69926317af61e6f53ff05af40a03cb254bf9762a
SOFTRE <39897273+SOFTRE@users.noreply.github.com>
2020-09-25 23:15:28 +0800

    one modify

---
+++ cloud-config-center-3344/src/main/java/com/xxm/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,11 @@
+package com.xxm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
